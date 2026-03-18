c5065323c22adbcd3f86518f4d534fb62e76441d
cantika-alff <cantikam822@gmail.com>
2026-03-19 01:02:01 +0700

    Create Guided Practice.dart

---
+++ Jobsheet1/Guided Practice.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Cantika Alifia Maharani";
+  String nim = "12345678";
+  String major = "Teknik Informatika";
+
+  print("Nama: $name");
+  print("NIM: $nim");
+  print("Jurusan: $major");
+}
